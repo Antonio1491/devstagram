db15904e413178757cbf2df4dff16c8e590fb6fb
Antonio De J. Góngora García <antonio.gongora1491@gmail.com>
2024-08-14 15:50:27 -0600

    validacion de formulario de edición perfil

--- app/Http/Controllers/PerfilController.php
+++ app/Http/Controllers/PerfilController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 
 class PerfilController extends Controller
@@ -13,5 +14,15 @@ class PerfilController extends Controller
     public function index()
     {
         return view('perfil.index');
+    }
+
+    public function store(Request $request)
+    {
+         //Modificar el Request para formatear un slug
+         $request->request->add(['username' => Str::slug($request->username)]);
+
+        $this->validate($request, [
+            'username' => ['required', 'unique:users,username,'.auth()->user()->id, 'min:3', 'max:20', 'not_in:editar-perfil'],
+        ]);
     }
 }
