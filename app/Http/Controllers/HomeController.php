51fb6524e438787d172ebb279ddc2767fd4e5d90
Antonio De J. Góngora García <antonio.gongora1491@gmail.com>
2024-08-16 16:35:04 -0600

    agregando middleware a página principal

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,11 +7,17 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
+
+    //No pueden ver la página principal si no estan autenticados
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function __invoke()
     {
 
         //obtener a quienes seguimos
-        // 
         $ids = auth()->user()->followings->pluck('id')->toArray();
         $posts= Post::whereIn('user_id', $ids)->latest()->paginate(20);
         
